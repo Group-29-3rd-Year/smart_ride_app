0ff5dce2e581906de4929dd7203298aecb8ae8e3
Janith Renuka <56946043+janithrenuka@users.noreply.github.com>
2021-06-18 22:10:38 +0530

    home page inti create

---
+++ lib/constraints.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+const SBackgroundColor = Color(0xFFFFFFF0);
+const STextColor = Color(0xFF222B45);
+const SGreyBackColor = Color(0xFFE1E1DD);
+const SShadowColor = Color(0xFFE6E6E6);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_ride_app/constraints.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Smart Ride Test',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: "Cairo",
+        scaffoldBackgroundColor: SGreyBackColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: STextColor)
       ),
       home: Home(),
     );
@@ -24,27 +27,157 @@ class Home extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
         return Scaffold(
+          resizeToAvoidBottomInset : false,
           body: Stack(
             children: <Widget>[
-              Container(
-                  
-              ),
-
               SafeArea(
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "Smart Ride",
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline4
-                              .copyWith(
-                                  fontWeight: FontWeight.w900
+                  padding: const EdgeInsets.all(2),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+
+                      Container(
+                        margin: EdgeInsets.only(top:25, ),
+                        child: Text(
+                              "Smart Ride",
+                              style: Theme.of(context)
+                                      .textTheme
+                                      .headline6
+                                      .copyWith(
+                                          fontWeight: FontWeight.w900,
+                                          fontSize: 40,
+                                      ),
+                            ),
+                      ),
+
+                      Container(
+                        margin: EdgeInsets.only(bottom: 30, left: 30, right: 30),
+                          height: size.height *0.35,
+                          width: size.width,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              alignment: Alignment.bottomLeft,
+                              image: AssetImage("assets/images/homeNew.png"),
+                              fit: BoxFit.fill,
+                            )
+                          ),
+                      ),
+                      
+                      Container(  //username
+                        margin: EdgeInsets.only(top:8, bottom: 5, left: 70, right: 70),
+                        padding: EdgeInsets.only(top: 0, bottom: 0, left: 15, right: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "User name",
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+
+                      Container( //password
+                        margin: EdgeInsets.symmetric(vertical: 0, horizontal: 70),
+                        padding: EdgeInsets.only(top: 0, bottom: 0, left: 15, right: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Password",
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+
+                      Container( //login button
+                        margin: EdgeInsets.only(top: 10),
+                        padding: EdgeInsets.only(top: 0, bottom: 0, left: 10, right: 10),
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(20),
+                          
+                        ),
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                            primary: Colors.white,
+                            textStyle: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text('Sign In'),
+                        ),
+                      ),
+
+                     
+                          Container(
+                            margin: EdgeInsets.only(top: 5),
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 5),
+                              child: Text(
+                                "Haven't account yet?",
+                                style: Theme.of(context)
+                                        .textTheme
+                                        .headline6
+                                        .copyWith(
+                                            fontWeight: FontWeight.w900,
+                                            fontSize: 20,
+                                        ),
                               ),
+                            ),
+                          ),
+
+                          Container( //signup text
+                            
+                            child: TextButton(
+                              style: TextButton.styleFrom(
+                                padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+                                primary: Colors.blue,
+                                textStyle: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              onPressed: () {},
+                              child: const Text('Sign Up'),
+                            ),
+                          ),
+                            
+                            Container(
+                              margin: EdgeInsets.only(top: 50),
+                              child: Padding(
+                                padding: const EdgeInsets.only(top: 15),
+                                child: Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Container(
+                                    child: Text(
+                                      "smartride © 2021",
+                                        style: Theme.of(context)
+                                          .textTheme
+                                          .headline6
+                                          .copyWith(
+                                              fontWeight: FontWeight.w900,
+                                              fontSize: 20,
+                                          ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                    ],
                   ),
-                )
-              )
+                ),
+              ),
             ],
           ),
       );
