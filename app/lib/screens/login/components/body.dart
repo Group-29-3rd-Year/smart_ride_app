1f158a6ae16199d42a3347d1013460796490963d
milanmalshika <milanmalshika22@gmail.com>
2021-08-08 08:28:55 +0530

    crate login backend in mobile app

--- app/lib/screens/login/components/body.dart
+++ app/lib/screens/login/components/body.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:smart_ride_app/components/already_have_an_account_check.dart';
@@ -7,12 +9,73 @@ import 'package:smart_ride_app/components/rounded_password_field.dart';
 import 'package:smart_ride_app/screens/login/components/background.dart';
 import 'package:smart_ride_app/screens/signup/signup_screen.dart';
 import 'package:smart_ride_app/screens/start_screen.dart';
+import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
 
-
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({
     Key key,
   }) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+
+  String email;
+  String pass;
+
+  Future login()async {
+
+    var url = "http://192.168.1.5:5002/add/login";
+    http.Response response = await http.post(
+      Uri.parse(url),
+      headers: <String,String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String,String>{
+          "email": email,
+          "password": pass,
+        })
+    );
+
+    var data = response.body;
+    
+    if (data== '"Success"') {
+      Fluttertoast.showToast(
+        msg: "Login Successfully",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 16.0
+      );
+
+      Navigator.push(
+        context, 
+          MaterialPageRoute(
+            builder: (context) {
+              return StartScreen();
+            },
+          ),
+      );
+
+    }
+    if(data=='"Error"') {
+        Fluttertoast.showToast(
+        msg: "Username or Password is Incorrect",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+      );
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,25 +104,26 @@ class Body extends StatelessWidget {
             SizedBox(height: size.height * 0.03,),
             RoundedInputField(
               hintText: "Your Email",
-              onChanged: (value) {},
+              onChanged: (value) {
+                setState((){
+                  email = value;
+                });
+              },
               icon: Icons.email,
             ),
 
             RoundedPasswordField(
-              onChanged: (value) {},
+              onChanged: (value) {
+                setState((){
+                  pass = value;
+                });
+              },
             ),
 
             RoundedButton(
               text: "LOGIN",
               press: () {
-                Navigator.push(
-                  context, 
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return StartScreen();
-                    },
-                  ),
-                );
+                login();
               },
             ),
 
